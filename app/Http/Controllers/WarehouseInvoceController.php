bc8ccf97054c707aa91da39c4daf6c18809cdba5
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-03-29 15:14:28 +0700

    Code Của Tĩnh

---
+++ app/Http/Controllers/WarehouseInvoceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Warehouse_Invoce;
+use Illuminate\Http\Request;
+
+class WarehouseInvoceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Warehouse_Invoce $warehouse_Invoce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Warehouse_Invoce $warehouse_Invoce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Warehouse_Invoce $warehouse_Invoce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Warehouse_Invoce $warehouse_Invoce)
+    {
+        //
+    }
+}
