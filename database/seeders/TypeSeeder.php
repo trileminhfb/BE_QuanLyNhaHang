16006fefc4bb5823db723cdb51d97463c3cbbe88
DT-VN <email@cuaban.com>
2025-05-05 18:21:48 +0700

    dat fix api

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('types')->insert([
+            [
+                'id_category' => 1,
+                'status' => 1,
+                'name' => 'Loại 1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_category' => 2,
+                'status' => 1,
+                'name' => 'Loại 2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_category' => 3,
+                'status' => 0,
+                'name' => 'Loại 3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
