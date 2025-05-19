e2246da50d7bb7fd7f2ba6adf299a8230d44bc00
trileminhfb <leminhtrisuper@gmail.com>
2025-05-19 13:14:00 +0700

    fetch

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,16 +10,28 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Rán', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Nướng', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Chiên Mắm', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Hấp', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Cay', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Xào', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Nấu Lẩu', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Sốt', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Phô Mai', 'created_at' => now(), 'updated_at' => now()],
-            ['id_type' => 1, 'status' => 1, 'name' => 'Gà Truyền Thống', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món nước', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món nướng', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món chiên', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món hấp', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món xào', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món mặn', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món ăn truyền thống', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món quốc tế', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món luộc', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món chay', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món ăn nhanh', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món khai vị', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món tráng miệng', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món ăn nhẹ', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món phụ', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 1, 'status' => 1, 'name' => 'Món ăn sáng', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Nước ngọt', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Nước ép', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Đồ uống có gas', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Đồ uống không cồn', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Đồ uống lạnh', 'created_at' => now(), 'updated_at' => now()],
+            ['id_type' => 2, 'status' => 1, 'name' => 'Đồ uống nóng', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
